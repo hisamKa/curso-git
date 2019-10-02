094f18b0181b6d0c5295f6e6b439392b87684968
Karen Santos <hisamitsu.moodle@gmail.com>
2019-10-02 13:48:16 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ projeto_Teste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace projeto_Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            Console.ReadLine();
+        }
+    }
+}
